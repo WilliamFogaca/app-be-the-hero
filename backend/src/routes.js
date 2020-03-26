fc7028edb081cd296cd36f7a605a0267ba83f14c
WilliamFogacaDev <williamps0810@gmail.com>
2020-03-26 18:54:18 -0300

    Dia 02 - Criação do backend em andamento. Criação das rotas/controllers dos incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const routes = express.Router();
 
+/* Controllers */
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
+/* ONGs Routes */
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+/* Incidents Routes */
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
