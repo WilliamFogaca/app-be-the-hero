8ee3dfbf19d2f99bbc6e6e8c6f9a12613e62f691
WilliamFogacaDev <williamps0810@gmail.com>
2020-03-26 19:42:55 -0300

    Dia 02 - Criação do backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,11 @@ const routes = express.Router();
 /* Controllers */
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+/* Session Routes */
+routes.post('/sessions', SessionController.create);
 
 /* ONGs Routes */
 routes.get('/ongs', OngController.index);
@@ -13,5 +18,8 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+/* Profile Routes */
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
