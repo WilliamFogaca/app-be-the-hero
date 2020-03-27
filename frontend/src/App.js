61c4a88d0eface37f84a76c36cd372d991191b6d
WilliamFogacaDev <williamps0810@gmail.com>
2020-03-27 14:57:44 -0300

    Dia 03 - Iniciando a construção do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Logon from './pages/Logon/index';
+
+import './global.css';
+
+function App() {
+  return (
+    <Logon />
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+const Logon = () => {
+  return (
+    <h1>Hello world</h1>
+  );
+}
+
+export default Logon;
